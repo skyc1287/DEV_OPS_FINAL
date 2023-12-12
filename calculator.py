a1eb5a0ee1d889d6bbd62eaae98daeafc8ae20ab
skyc1287 <sky.c.cerv@gmail.com>
2023-12-12 12:42:45 -0700

    update

--- calculator.py
+++ calculator.py
@@ -29,7 +29,7 @@ print("4.Divide")
 
 choice = input("Enter choice(1/2/3/4): ")
 
-num1 = float(input("Enter first number:  "))
+num1 = float(input("Enter first number:   "))
 num2 = float(input("Enter second number:  "))
 
 if choice == '1':
